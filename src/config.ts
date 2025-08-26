fdc0b5fad7168b1c0503cea7b2e57b4145f8d0e6
cerebralawakening69 <cerebralawakening69@gmail.com>
2025-08-26 13:44:23 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // URL do Apps Script (sua /exec)
 export const WEB_APP_URL =
-  "https://script.google.com/macros/s/AKfycbxfESf6kfdeq_qjoy3GMY50GqDOZ5fVa5tOTqYHm_o_AdY3-W97xKBdc59udTB1tm8/exec";
+https://script.google.com/macros/s/AKfycbz0kI5oTcPGYKJqJ-LEm_AHINNQbpwfmUvXIgj8wxFl_jG2yg0-6dz8aSL2CfD4lEY/exec
 
 // Seus links de afiliado
 export const AFF_LINK_EMAIL =

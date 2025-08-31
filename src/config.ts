8accd6258e54867c0a75c0d2972af04edee893d2
cerebralawakening69 <cerebralawakening69@gmail.com>
2025-08-31 10:23:54 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,5 @@
 // URL do Apps Script (sua /exec)
-export const WEB_APP_URL =
-https://script.google.com/macros/s/AKfycbz0kI5oTcPGYKJqJ-LEm_AHINNQbpwfmUvXIgj8wxFl_jG2yg0-6dz8aSL2CfD4lEY/exec
-
+export const WEB_APP_URL = https://script.google.com/macros/s/AKfycbx46lRFF4UZ9R5M1BPTyt-PrXSMHxqCb_xb1yYfJgyWq__qN2DD6N1gKPXgWUpiUg/exec
 // Seus links de afiliado
 export const AFF_LINK_EMAIL =
   "https://pxt.pinealxt.com/ds/presentation/index.php#aff=awakeningprotocol&cam=email";

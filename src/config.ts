2c49d778f69f1c1ef4d300ae15f8ae714564a02b
cerebralawakening69 <cerebralawakening69@gmail.com>
2025-09-01 21:58:37 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
+// src/config.ts
 export const WEB_APP_URL =
-  "https://script.google.com/macros/s/AKfycbx46lRFF4UZ9R5M1BPTyt-PrXSMHxqCb_xb1yYfJgyWq__qN2DD6N1gKPXgWUpiUg/exec";
+  "https://script.google.com/macros/s/AKfycbzqkAXp2WSz5C77RRg73XRFzyjtX3Iz9RzLPkZWSmV1h3Kuk3qnuayd7LM5Jbls_PA8/exec";
+
 // Seus links de afiliado
 export const AFF_LINK_EMAIL =
   "https://pxt.pinealxt.com/ds/presentation/index.php#aff=awakeningprotocol&cam=email";
